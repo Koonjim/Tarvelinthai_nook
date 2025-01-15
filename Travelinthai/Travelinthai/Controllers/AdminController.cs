a8a9fa9cf99f7ebfbc897df07f46ccaaaab97d30
Koonjim <jim84.j@gmail.com>
2025-01-15 21:15:12 +0700

    151

--- Travelinthai/Travelinthai/Controllers/AdminController.cs
+++ Travelinthai/Travelinthai/Controllers/AdminController.cs
@@ -8,12 +8,13 @@ namespace Travelinthai.Controllers
 {
     public class AdminController : Controller
     {
-       
+
         private readonly ApplicationDbContext _context;
         public AdminController(ApplicationDbContext context)
         {
             _context = context;
         }
+
         public IActionResult MesasureAdmin()
         {
             IEnumerable<Location_tb> locations = _context.Location_tb
@@ -29,35 +30,64 @@ namespace Travelinthai.Controllers
                 .Where(b => b.TypeID == 2)
                 .ToList();
             return View(locations);
-        }public IActionResult MountainAdmin()
+        }
+        public IActionResult MountainAdmin()
         {
             IEnumerable<Location_tb> locations = _context.Location_tb
                 .Include(b => b.Type)
                 .Where(b => b.TypeID == 3)
                 .ToList();
-
             return View(locations);
-        }public IActionResult WaterfallAdmin()
+        }
+        public IActionResult WaterfallAdmin()
         {
             IEnumerable<Location_tb> locations = _context.Location_tb
                 .Include(b => b.Type)
                 .Where(b => b.TypeID == 4)
                 .ToList();
-
             return View(locations);
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
+            
+            var Location = _context.Location_tb.Find(id);
+            if (Location == null)
+            {
+                return View();
+            }
+
+            _context.Location_tb.Remove(Location);
+            _context.SaveChanges();
+            return RedirectToAction("TypeAdmin", "Type");
+        }
+        
+        public IActionResult Edit(int id)
+        {
+            //เช็ก LocationID ว่ามีค่าเป็น0หรือไม่มีค่าหรือป่าว
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
             var Location = _context.Location_tb.Find(id);
             if (Location == null)
             {
                 return NotFound();
             }
-
-            _context.Location_tb.Remove(Location);
+            
+            return View(Location);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Location_tb Location)
+        {
+            if (Location == null)
+            {  
+                return View(Location);
+            }
+            _context.Update(Location);
             _context.SaveChanges();
             return RedirectToAction("TypeAdmin", "Type");
         }
